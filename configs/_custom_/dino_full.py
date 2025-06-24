6efab64bd4827257b001924c767446c281845c08
MT121201 <kieu.triet1@gmail.com>
2025-06-24 09:54:58 +0000

    update

--- configs/_custom_/dino_full.py
+++ configs/_custom_/dino_full.py
@@ -1,4 +1,5 @@
-_base_ = '/home/a3ilab01/treeai/mmdetection/work_dirs/dino_34b/best_coco_bbox_mAP_50_epoch_20.pth'
+_base_ = '../dino/dino-5scale_swin-l_8xb2-12e_coco.py'
+# load_from = '/home/a3ilab01/treeai/mmdetection/work_dirs/dino_34b/best_coco_bbox_mAP_50_epoch_20.pth'
 
 max_epochs = 36
 train_cfg = dict(
@@ -21,14 +22,52 @@ param_scheduler = [
 dataset_type = 'CocoDataset'
 data_root = '/home/a3ilab01/treeai/dataset/'
 
-classes = tuple(f'cls_{i}' for i in range(1, 62))
-metainfo = dict(classes=classes)
+dataset12_classes = [
+    "betula papyrifera", "tsuga canadensis", "picea abies", "acer saccharum",
+    "betula sp.", "pinus sylvestris", "picea rubens", "betula alleghaniensis",
+    "larix decidua", "fagus grandifolia", "picea sp.", "fagus sylvatica",
+    "dead tree", "acer pensylvanicum", "populus balsamifera", "quercus ilex",
+    "quercus robur", "pinus strobus", "larix laricina", "larix gmelinii",
+    "pinus pinea", "populus grandidentata", "pinus montezumae", "abies alba",
+    "betula pendula", "pseudotsuga menziesii", "fraxinus nigra",
+    "dacrydium cupressinum", "cedrus libani", "acer pseudoplatanus",
+    "pinus elliottii", "cryptomeria japonica", "pinus koraiensis",
+    "abies holophylla", "alnus glutinosa", "fraxinus excelsior", "coniferous",
+    "eucalyptus globulus", "pinus nigra", "quercus rubra", "tilia europaea",
+    "abies firma", "acer sp.", "metrosideros umbellata", "acer rubrum",
+    "picea mariana", "abies balsamea", "castanea sativa", "tilia cordata",
+    "populus sp.", "crataegus monogyna", "quercus petraea", "acer platanoides"
+]
+
+metainfo = dict(classes=dataset12_classes)
 
 model = dict(
     bbox_head=dict(
-        num_classes=len(classes)
+        type='DINOHead',
+        num_classes=len(dataset12_classes),  
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0
+        ),
+        loss_bbox=dict(type='L1Loss', loss_weight=7.0),  # was 5.0
+        loss_iou=dict(type='CIoULoss', loss_weight=3.0),  # was GIoULoss
+        train_cfg=dict(
+            assigner=dict(
+                type='HungarianAssigner',
+                match_costs=[
+                    dict(type='FocalLossCost', weight=2.0),
+                    dict(type='BBoxL1Cost', weight=7.0, box_format='xywh'),
+                    dict(type='CIoUCost', weight=3.0)
+                ]
+            )
+        ),
+        test_cfg=dict(max_per_img=300)
     )
 )
+
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -38,7 +77,6 @@ train_pipeline = [
     dict(type='Resize', scale=(640, 640), keep_ratio=True),
     dict(type='PackDetInputs')
 ]
-
 
 test_pipeline = [
     dict(backend_args=None, type='LoadImageFromFile'),
@@ -63,10 +101,10 @@ datasets = [
     )
     for subdir in [
         '12_RGB_ObjDet_640_fL',
-        '5_RGB_S_320_pL',
-        '34_RGB_ObjDet_640_pLa',
-        '34_RGB_ObjDet_640_pLb',
-        '0_RGB_fL/coco'  # assuming already filtered and remapped
+        # '5_RGB_S_320_pL',
+        '34_RGB_ObjDet_640_pL',
+        '34_RGB_ObjDet_640_pL_b',
+        # '0_RGB_FullyLabeled/coco'  # assuming already filtered and remapped
     ]
 ]
 
@@ -75,21 +113,37 @@ train_dataloader = dict(
     batch_size=2,
     dataset=dict(
         type='RepeatDataset',
-        times=2,
+        times=1,
         dataset=dict(
-            type='ClassBalancedDataset',
-            oversample_thr=1e-2,
-            dataset=dict(
-                type='ConcatDataset',
-                datasets=datasets
-            )
-        )
+        type='ConcatDataset',
+        datasets=[
+            dict(
+                type='ClassBalancedDataset',
+                oversample_thr=1e-3,
+                dataset=ds
+            ) for ds in datasets
+    ]
+)
     ),
     sampler=dict(type='DefaultSampler', shuffle=True),
     batch_sampler=dict(type='AspectRatioBatchSampler'),
     num_workers=2,
     persistent_workers=True
 )
+
+# train_dataloader = dict(
+#     _delete_=True,
+#     batch_size=2,
+#     dataset=dict(
+#         type='ConcatDataset',
+#         datasets=datasets  # direct concat of all datasets
+#     ),
+#     sampler=dict(type='DefaultSampler', shuffle=True),
+#     batch_sampler=dict(type='AspectRatioBatchSampler'),
+#     num_workers=2,
+#     persistent_workers=True
+# )
+
 
 val_dataloader = dict(
     _delete_=True,
